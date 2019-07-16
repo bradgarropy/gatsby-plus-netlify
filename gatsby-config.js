1ec4f52ebd820ce8ca5cba37bf9cb944aac9a966
Brad Garropy <bradgarropy@gmail.com>
2019-07-16 16:02:29 -0500

    transform markdown.

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
         path: `${__dirname}/content/agendas`,
       },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
