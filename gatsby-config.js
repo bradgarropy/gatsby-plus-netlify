5f4bdaf266b5a5577184a1271a0c64a079e1fcb4
Brad Garropy <bradgarropy@gmail.com>
2019-07-16 15:57:49 -0500

    source agendas.

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `agendas`,
+        path: `${__dirname}/content/agendas`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
