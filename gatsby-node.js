2f231d3dc687607f9b60ef1ebb9b8dd062b01940
Brad Garropy <bradgarropy@gmail.com>
2019-07-17 10:50:53 -0500

    create agenda page.

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,19 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require("path")
+
+const createPages = ({ actions }) => {
+  const { createPage } = actions
+
+  createPage({
+    path: "/agenda",
+    component: path.resolve("src/templates/agenda.js"),
+    context: {
+      name: "value",
+    },
+  })
+}
+
+module.exports = { createPages }
---
+++ src/templates/agenda.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Agenda = ({ pageContext }) => {
+  console.log(pageContext)
+
+  return (
+    <div>
+      <h1>Agenda</h1>
+    </div>
+  )
+}
+
+export default Agenda
