c8e8271cd7e7f61113ffe53072af9a9877ee4604
Brad Garropy <bradgarropy@gmail.com>
2019-07-17 10:18:43 -0500

    display agendas.

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,12 +23,16 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
+
+      <h1>Agendas</h1>
+      {data.allFile.edges.map((edge, index) => (
+        <p key={index}>{edge.node.name}</p>
+      ))}
+
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
+
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
