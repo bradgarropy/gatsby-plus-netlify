f912e5d4e685a83b963da7a370beb5cb4e185b79
Brad Garropy <bradgarropy@gmail.com>
2019-07-17 11:16:59 -0500

    create all agenda pages.

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,15 +8,29 @@
 
 const path = require("path")
 
-const createPages = ({ actions }) => {
+const createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
-  createPage({
-    path: "/agenda",
-    component: path.resolve("src/templates/agenda.js"),
-    context: {
-      name: "value",
-    },
+  const response = await graphql(`
+    query READ_AGENDAS {
+      allFile(filter: { sourceInstanceName: { eq: "agendas" } }) {
+        edges {
+          node {
+            name
+          }
+        }
+      }
+    }
+  `)
+
+  response.data.allFile.edges.forEach(edge => {
+    createPage({
+      path: `/agenda/${edge.node.name}`,
+      component: path.resolve("src/templates/agenda.js"),
+      context: {
+        name: edge.node.name,
+      },
+    })
   })
 }
 
