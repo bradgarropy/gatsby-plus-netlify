31b5c83ce90d28a94cb5cdf7c4aaa97f65c26100
Brad Garropy <bradgarropy@gmail.com>
2019-07-17 11:38:38 -0500

    query agenda.

--- src/templates/agenda.js
+++ src/templates/agenda.js
@@ -1,7 +1,19 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-const Agenda = ({ pageContext }) => {
+export const query = graphql`
+  query READ_AGENDA($name: String!) {
+    file(name: { eq: $name }) {
+      childMarkdownRemark {
+        html
+      }
+    }
+  }
+`
+
+const Agenda = ({ pageContext, data }) => {
   console.log(pageContext)
+  console.log(data)
 
   return (
     <div>
